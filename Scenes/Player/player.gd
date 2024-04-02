0eb8750307b69429955f8c0afaacbe7a51dcded1
Willian Gigliotti <wgigliotti@gmail.com>
2024-04-02 13:31:50 -0300

    Reoganização do player para multiplayer

--- Scenes/Player/player.gd
+++ Scenes/Player/player.gd
@@ -2,11 +2,6 @@ extends RigidBody2D
 
 @export var bullet_scene: PackedScene
 
-@export var left: String
-@export var right: String
-@export var up: String
-@export var shoot_key: String
-
 var acceleration = 200
 var velocity = Vector2.DOWN 
 var rotation_speed = 3.5
@@ -22,7 +17,7 @@ func shoot():
 	print("shooting")
 	var bullet = bullet_scene.instantiate()
 	
-	var cannon = $CannonLeft.position.rotated(rotation) if cannon_left else $CannonRight.position.rotated(rotation)
+	var cannon = $Cannons/CannonLeft.position.rotated(rotation) if cannon_left else $Cannons/CannonRight.position.rotated(rotation)
 	cannon_left = !cannon_left
 	
 	print(cannon_left)
@@ -35,25 +30,23 @@ func shoot():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 
-func jet(particle, button, propNode):
+func jet(particle, pressed, propNode):
 	particle.emitting = false	
-	if Input.is_action_pressed(button):		
+	if pressed:		
 		particle.emitting = true		
 		apply_force(Vector2.DOWN.rotated(rotation)*80000, propNode.get_position().rotated(rotation))
 	
 	
 func _process(delta):	
-	jet($LeftProp/Jet, left, $LeftProp)
-	jet($RightProp/Jet, right, $RightProp)
+	jet($Jets/LeftProp/Jet, $Controls.left, $Jets/LeftProp)
+	jet($Jets/RightProp/Jet, $Controls.right, $Jets/RightProp)
 	
 	
-	if Input.is_action_pressed(shoot_key):
+	if $Controls.shoot:
 		if can_shoot == true:
 			can_shoot = false
-			$ShootTimer.start()
+			$Cannons/ShootTimer.start()
 			shoot()
-	
-	
 
 
 func _on_timer_timeout():
